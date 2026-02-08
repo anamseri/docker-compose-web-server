44cc647ba81c5aeadb3f750d1492822076400eb9
anamseri <anamserni@gmail.com>
2026-02-08 08:56:03 +0700

    Update Setup

--- set-permissions.sh
+++ set-permissions.sh
@@ -8,7 +8,6 @@ if [ ! -f "$ENV_FILE" ]; then
     exit 1
 fi
 
-# Load env safely
 set -a
 source "$ENV_FILE"
 set +a
@@ -19,8 +18,6 @@ if [ -z "$PROJECT_DIR" ]; then
 fi
 
 USER_NAME="$USER"
-# SSL_KEY="$PROJECT_DIR/nginx/certbot/live/localhost/privkey.pem"
-# SSL_CERT="$PROJECT_DIR/nginx/certbot/live/localhost/fullchain.pem"
 
 echo "Set ownership to user: $USER_NAME"
 sudo chown -R $USER_NAME:$USER_NAME "$PROJECT_DIR"
@@ -32,13 +29,6 @@ echo "Set file permissions: 644"
 sudo find "$PROJECT_DIR" -type f -exec chmod 644 {} \;
 
 echo "Set permission .env: 600"
-# chmod 600 .env
 chmod 600 "$ENV_FILE"
-
-# echo "Set permission SSL private key: 600"
-# [ -f "$SSL_KEY" ] && chmod 600 "$SSL_KEY"
-
-# echo "Set permission SSL public certificate: 644"
-# [ -f "$SSL_CERT" ] && chmod 644 "$SSL_CERT"
 
 echo "Permissions setup complete!."
